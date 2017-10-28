d09f1da7827580e5337984b4b2f848af32fe634e
JJ Posti <jjposti@techtimejourney.net>
2017-10-28 22:03:19 +0300

    Calculator v.1 upload

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,20 @@
+/*
+Calculator v.1 Copyright (c) 2017 JJ Posti <techtimejourney.net> This program comes with ABSOLUTELY NO WARRANTY; 
+for details see: http://www.gnu.org/copyleft/gpl.html. This is free software, and you are welcome to redistribute it under 
+GPL Version 2, June 1991")
+ */
+package calculator;
+
+/**
+ *
+ * @author JJ Posti - techtimejourney.net.
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Gui gui = new Gui();
+    gui.setVisible(true);}
+}
